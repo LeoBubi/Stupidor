54bb16cee418b34af9aeb823991d3e6d29399c49
Leonardo Brugnano <lebrug504@gmail.com>
2024-04-16 14:42:43 +0200

    Add check_username.c

---
+++ src/check_username.c
@@ -0,0 +1,21 @@
+#include "includes.h"
+
+int check_username(const char* username) {
+    FILE* file = fopen("/var/stupidor/users", "r");
+    if (file == NULL) {
+        printf("Error opening users file.\n");
+        return -1;
+    }
+
+    char line[URECSIZ];
+    while (fgets(line, URECSIZ, file)) {
+        char* token = strtok(line, ":");
+        if (token != NULL && strcmp(token, username) == 0) {
+            fclose(file);
+            return 1; // Username found
+        }
+    }
+
+    fclose(file);
+    return 0; // Username not found
+}
