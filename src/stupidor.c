33b4c5ccdda3c09add2b6fa0cc661bde23196ee7
Leonardo Brugnano <lebrug504@gmail.com>
2024-04-16 13:52:28 +0200

    Add stupidor.c file with command line interface implementation

---
+++ src/stupidor.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* Check if the program is run as root */
+    if (geteuid() != 0) {
+        printf("This program must be run as root.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Print help info (?) */
+    if (argc == 1 || (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))) {
+        FILE *file = fopen("/usr/local/share/doc/stupidor/stupidor.txt", "r");
+        if (file == NULL) {
+            printf("Failed to open help file.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        char line[256];
+        while (fgets(line, sizeof(line), file) != NULL) {
+            printf("%s", line);
+        }
+
+        fclose(file);
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Print version info (?) */
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) {
+        printf("stupidor 1.0\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Check command */
+    if      (strcmp(argv[1], "send") == 0)
+        stupidor_send();
+    else if (strcmp(argv[1], "inbox") == 0)
+        stupidor_inbox();
+    else if (strcmp(argv[1], "signin") == 0)
+        stupidor_signin();
+    else if (strcmp(argv[1], "signout") == 0)
+        stupidor_signout();
+    else if (strcmp(argv[1], "signup") == 0)
+        stupidor_signup();
+    else {
+        printf("Invalid command.\n");
+        exit(EXIT_FAILURE);
+    }
+}
