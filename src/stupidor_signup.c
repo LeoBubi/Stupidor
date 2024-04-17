557997214677a976474b842235815833b18e414b
Leonardo Brugnano <lebrug504@gmail.com>
2024-04-17 09:28:08 +0200

    Refactor input handling in stupidor_signup.c

--- src/stupidor_signup.c
+++ src/stupidor_signup.c
@@ -10,15 +10,16 @@ void stupidor_signup() {
     /* enter username */
     printf("Enter username: ");
     fgets(username, UNAMEMAX, stdin);
-    username[strcspn(username, "\n")] = '\0'; // remove trailing newline
-    { int c; while ((c = getchar()) != '\n' && c != EOF) {} }
+    if (username[strlen(username) - 1] == '\n') {
+        username[strlen(username) - 1] = '\0'; // remove trailing newline
+    } else { int c; while ((c = getchar()) != '\n' && c != EOF) {} } // flush stdin
 
     /* enter password */
     printf("Enter password: ");
     fgets(password, PASSDMAX, stdin);
-    password[strcspn(password, "\n")] = '\0'; // remove trailing newline
-    /* flush stdin */
-    { int c; while ((c = getchar()) != '\n' && c != EOF) {} }
+    if (password[strlen(password) - 1] == '\n') {
+        password[strlen(password) - 1] = '\0'; // remove trailing newline
+    } else { int c; while ((c = getchar()) != '\n' && c != EOF) {} } // flush stdin
 
     /* check if username is already taken */
     if (check_username(username)) {
