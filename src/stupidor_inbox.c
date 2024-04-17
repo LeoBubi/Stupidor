9d724e05d85a5da81edbd3ff0523c6530d2ff540
Leonardo Brugnano <lebrug504@gmail.com>
2024-04-17 09:59:13 +0200

    Add stupidor_inbox function to check inbox

---
+++ src/stupidor_inbox.c
@@ -0,0 +1,64 @@
+/*
+Check inbox
+
+Usage:
+    stupidor inbox
+
+Flags:
+    -h, --help          Show this help info
+    -d, --delete ID     Delete a message in the inbox
+    -D, --delete-all    Delete all messages in the inbox
+*/
+
+#include "includes.h"
+
+void stupidor_inbox(void)
+{
+    char username[UNAMEMAX];
+    memset(username, '\0', UNAMEMAX);
+    char password[PASSDMAX];
+    memset(password, '\0', PASSDMAX);
+
+    /* enter username */
+    printf("Username: ");
+    fgets(username, UNAMEMAX, stdin);
+    if (username[strlen(username) - 1] == '\n') {
+        username[strlen(username) - 1] = '\0'; // remove trailing newline
+    } else { int c; while ((c = getchar()) != '\n' && c != EOF) {} } // flush stdin
+
+    /* check if username exists */
+    if (!check_username(username)) {
+        printf("Username does not exist.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* enter password */
+    printf("Password: ");
+    fgets(password, PASSDMAX, stdin);
+    if (password[strlen(password) - 1] == '\n') {
+        password[strlen(password) - 1] = '\0'; // remove trailing newline
+    } else { int c; while ((c = getchar()) != '\n' && c != EOF) {} } // flush stdin
+
+    /* check if password is correct */
+    if (!check_password(username, password)) {
+        printf("Incorrect password.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* read inbox */
+    char filepath[PATH_MAX];
+    snprintf(filepath, sizeof(filepath), "/var/stupidor/%s", username);
+    FILE* user_file = fopen(filepath, "r");
+    if (user_file == NULL) {
+        printf("Error opening inbox.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MSGMAX+UNAMEMAX+3]; // +3 for ' ', ':' and '\n'
+    while (fgets(line, sizeof(line), user_file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(user_file);
+    exit(EXIT_SUCCESS);
+}
